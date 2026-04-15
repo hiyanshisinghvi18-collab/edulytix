26d6abe49410d8458d07f2b7fa6591e5448712a0
hiyanshisinghvi18-collab <hiyanshisinghvi18@gmail.com>
2026-04-16 00:48:34 +0530

    Add files via upload

---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,10 @@
+// export const environment = {
+//   production: false,
+//   apiUrl: 'http://localhost:8080/api',
+// };
+
+
+export const environment = {
+  production: false,
+  apiUrl: 'https://edulytix-backend.onrender.com/api', // /api
+};
